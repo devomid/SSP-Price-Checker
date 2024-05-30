e0d684037a017d186e561f34dc571c6a1bc50522
Omid Azad <mail.dev.om@gmail.com>
2024-05-30 17:53:19 +0330

    version number changed

--- src/dialogs/about.js
+++ src/dialogs/about.js
@@ -28,7 +28,7 @@ const About = ({ open, setOpen }) => {
                                 SSP Price Calculation Utility{'\n'}
                                 created and developed by: Omid Azad{'\n'}
                                 Date created: May 5th 2024{'\n'}
-                                Ver 2.1.2
+                                Ver 2.1.3
                             </Typography>
                         </Box>
                     </Box>
